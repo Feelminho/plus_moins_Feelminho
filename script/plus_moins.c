37fb561b6899da7f6a21eba0bc31b46319c90f16
Feelminho <minhoyamza007@gmail.com>
2022-07-22 12:57:37 +0400

    [v0.2]mode de difficulté

--- script/plus_moins.c
+++ script/plus_moins.c
@@ -15,12 +15,22 @@ int main ()
 {
 	srand(time(NULL));
 	int a=rand()%100;
-	int nenazy=0;
+	int nenazy, limit,essai,valiny;
+	printf("Ino tianao basy?\n 1-Facile\n 2-Moyen\n 3-Difficile\n");
+	scanf("%d",&valiny);
+	if (valiny==1)
+		limit = 1000000000;
+	else if (valiny==2)
+		limit=25;
+	else
+		limit=10;
+	essai=0;
 
-	while (nenazy != a)
+	while ((essai<=limit) && (nenazy!=a))
 	{
 	printf("Devine le chiffre auquel je pense basy\n");
 	scanf("%d",&nenazy);
 	plus_moins(nenazy,a);
+	essai++;
 	}
 }
